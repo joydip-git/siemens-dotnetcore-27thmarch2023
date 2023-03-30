0bbe67217e39498dd3e5c8485a9cf8ef935bb309
Joydip <joydip.mondal@gmail.com>
2023-03-30 17:33:39 +0530

    day-4 end of session update

---
+++ codes/day-4/Collections_Demo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Demo
+{
+    class Person //Object
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}, {Id}";
+        }
+        //Object class's original GetHasCode (Virtual) clculates hash code based on reference
+        //You need to override that GetHashCode
+        public override int GetHashCode()
+        {
+            int hash = this.Id.GetHashCode();
+            return hash;
+        }
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+            if (this == person)
+            {
+                return true;
+            }
+
+            if (this.Id != person.Id)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            /*
+            List<int> list = new List<int>();
+            list.Add(1); //0
+            list.Add(2); //1
+            list.Add(3); //2
+            list.Add(4);
+            list.Add(6);
+            list.Add(7);
+            list.Add(3);
+            list.Add(8);
+            list.Add(9); //8
+            list.Insert(1, 11); //index <= count
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Capacity);
+            Console.WriteLine("\n");
+            //list.Remove(11);
+            //list.RemoveAt(1);
+            list.Sort();
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //list.Add('a');
+            Console.WriteLine("\n");
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1);
+            set.Add(2);
+            set.Add(1);
+            foreach (int item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n");
+            */
+            HashSet<Person> people = new HashSet<Person>();
+            //Object initializer (3.0 -2007)
+            Person first = new Person { Id = 1, Name = "anil" };
+            Person second = new Person { Id = 2, Name = "sunil" };
+            Person third = new Person { Id = 1, Name = "anil" };
+
+            people.Add(first); //first.GetHashCode() => first.Equals(other)
+            people.Add(second);
+            people.Add(third);
+
+            foreach (Person item in people)
+            {
+                //Console.WriteLine(item.ToString());
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ codes/day-4/GenericsDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    //TData=> Type Parameter
+    //where TData : class => constraint
+    interface IWriter<TData> where TData : class
+    {
+        bool WriteData(TData data);
+    }
+    interface IReader<TData>
+    {
+        TData ReadData();
+    }
+    class StringWriter : IWriter<string>
+    {
+        public bool WriteData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class StringReader : IReader<string>
+    {
+        public string ReadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Calculation<TResult> where TResult : struct
+    {
+        public TResult Add<TInput>(TInput first, TInput second) where TInput : struct
+        {
+            return default(TResult);
+        }
+        public TResult Add<T1, T2>(T1 first, T2 second) where T1 : struct where T2 : struct
+        {
+            return default(TResult);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Calculation<long> calc = new Calculation<long>();
+            calc.Add<int>(12, 13);
+            //calc.Add<string>("siemens", "healthineers");
+            calc.Add<int, long>(12, 1000000000);
+        }
+    }
+}
